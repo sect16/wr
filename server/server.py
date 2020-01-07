e64cbc6aee796c95179314b894e436615616a652
pi <89375638+sect16@users.noreply.github.com>
2020-01-07 04:14:03 +0000

    Reformat code.

--- server/server.py
+++ server/server.py
@@ -6,20 +6,21 @@
 # Author      : William
 # Date        : 2019/02/23
 
-import socket
-import time
-import threading
-import move
-import Adafruit_PCA9685
-from rpi_ws281x import *
 import argparse
 import os
-import ultra
+import socket
+import threading
+import time
+
+import Adafruit_PCA9685
 import FPV
-import psutil
-import servo
 # import LED
 import findline
+import move
+import psutil
+import servo
+import ultra
+from rpi_ws281x import *
 
 step_set = 1
 speed_set = 100
@@ -42,11 +43,12 @@ sport_mode_on = 0
 SpeedBase = 70
 
 
-def findline_thread():       #Line tracking mode
+def findline_thread():  # Line tracking mode
     while 1:
         while FindLineMode:
             findline.run()
         time.sleep(0.2)
+
 
 def get_cpu_tempfunc():
     """ Return CPU temperature """
@@ -56,7 +58,7 @@ def get_cpu_tempfunc():
         for line in mytmpfile:
             result = line
 
-    result = float(result)/1000
+    result = float(result) / 1000
     result = round(result, 1)
     return str(result)
 
@@ -86,7 +88,7 @@ def get_swap_info():
 
 
 def info_get():
-    global cpu_t,cpu_u,gpu_t,ram_info
+    global cpu_t, cpu_u, gpu_t, ram_info
     while 1:
         cpu_t = get_cpu_tempfunc()
         cpu_u = get_cpu_use()
@@ -96,14 +98,14 @@ def info_get():
 
 def info_send_client():
     SERVER_IP = addr[0]
-    SERVER_PORT = 2256   #Define port serial 
+    SERVER_PORT = 2256  # Define port serial
     SERVER_ADDR = (SERVER_IP, SERVER_PORT)
-    Info_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Set connection value for socket
+    Info_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Set connection value for socket
     Info_Socket.connect(SERVER_ADDR)
     print(SERVER_ADDR)
     while 1:
         try:
-            Info_Socket.send((get_cpu_tempfunc()+' '+get_cpu_use()+' '+get_ram_info()).encode())
+            Info_Socket.send((get_cpu_tempfunc() + ' ' + get_cpu_use() + ' ' + get_ram_info()).encode())
             time.sleep(1)
         except:
             pass
@@ -111,19 +113,19 @@ def info_send_client():
 
 def ultra_send_client():
     ultra_IP = addr[0]
-    ultra_PORT = 2257   #Define port serial 
+    ultra_PORT = 2257  # Define port serial
     ultra_ADDR = (ultra_IP, ultra_PORT)
-    ultra_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Set connection value for socket
+    ultra_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Set connection value for socket
     ultra_Socket.connect(ultra_ADDR)
     print(ultra_ADDR)
     while 1:
         while ultrasonicMode:
             try:
                 if not FindColorMode:
-                    ultra_Socket.send(str(round(ultra.checkdist(),2)).encode())
+                    ultra_Socket.send(str(round(ultra.checkdist(), 2)).encode())
                     time.sleep(0.5)
                     continue
-                fpv.UltraData(round(ultra.checkdist(),2))
+                fpv.UltraData(round(ultra.checkdist(), 2))
                 time.sleep(0.2)
             except:
                 pass
@@ -131,11 +133,11 @@ def ultra_send_client():
 
 
 def FPV_thread():
-    fpv=FPV.FPV()
+    fpv = FPV.FPV()
     fpv.capture_thread(addr[0])
 
 
-def  ap_thread():
+def ap_thread():
     os.system("sudo create_ap wlan0 eth0 AdeeptCar 12345678")
 
 
@@ -144,18 +146,18 @@ def run():
     move.setup()
     findline.setup()
 
-    info_threading=threading.Thread(target=info_send_client)    #Define a thread for FPV and OpenCV
-    info_threading.setDaemon(True)                             #'True' means it is a front thread,it would close when the mainloop() closes
-    info_threading.start()                                     #Thread starts
+    info_threading = threading.Thread(target=info_send_client)  # Define a thread for FPV and OpenCV
+    info_threading.setDaemon(True)  # 'True' means it is a front thread,it would close when the mainloop() closes
+    info_threading.start()  # Thread starts
 
-    ultra_threading=threading.Thread(target=ultra_send_client)    #Define a thread for FPV and OpenCV
-    ultra_threading.setDaemon(True)                             #'True' means it is a front thread,it would close when the mainloop() closes
-    ultra_threading.start()                                     #Thread starts
+    ultra_threading = threading.Thread(target=ultra_send_client)  # Define a thread for FPV and OpenCV
+    ultra_threading.setDaemon(True)  # 'True' means it is a front thread,it would close when the mainloop() closes
+    ultra_threading.start()  # Thread starts
 
-    findline_threading=threading.Thread(target=findline_thread)    #Define a thread for FPV and OpenCV
-    findline_threading.setDaemon(True)                             #'True' means it is a front thread,it would close when the mainloop() closes
-    findline_threading.start()                                     #Thread starts
-    #move.stand()
+    findline_threading = threading.Thread(target=findline_thread)  # Define a thread for FPV and OpenCV
+    findline_threading.setDaemon(True)  # 'True' means it is a front thread,it would close when the mainloop() closes
+    findline_threading.start()  # Thread starts
+    # move.stand()
 
     ws_R = 0
     ws_G = 0
@@ -165,7 +167,7 @@ def run():
     Y_pitch_MAX = 200
     Y_pitch_MIN = -200
 
-    while True: 
+    while True:
         data = ''
         data = str(tcpCliSock.recv(BUFSIZ).decode())
         if not data:
@@ -192,7 +194,10 @@ def run():
                 move.move(SpeedBase, direction_command, turn_command, rad)
         elif 'DS' in data:
             direction_command = 'no'
-            move.move(speed_set, direction_command, turn_command, rad)
+            if SportModeOn:
+                move.move(speed_set, direction_command, turn_command, rad)
+            else:
+                move.move(SpeedBase, direction_command, turn_command, rad)
 
         elif 'left' == data:
             turn_command = 'left'
@@ -211,31 +216,31 @@ def run():
             move.move(speed_set, direction_command, turn_command, rad)
 
         elif 'headup' == data:
-            servo.camera_ang('lookup','no')
+            servo.camera_ang('lookup', 'no')
         elif 'headdown' == data:
-            servo.camera_ang('lookdown','no')
+            servo.camera_ang('lookdown', 'no')
         elif 'headhome' == data:
-            servo.camera_ang('home','no')
+            servo.camera_ang('home', 'no')
             time.sleep(0.2)
             servo.clean_all()
 
         elif 'wsR' in data:
             try:
-                set_R=data.split()
+                set_R = data.split()
                 ws_R = int(set_R[1])
                 # LED.colorWipe(Color(ws_R,ws_G,ws_B))
             except:
                 pass
         elif 'wsG' in data:
             try:
-                set_G=data.split()
+                set_G = data.split()
                 ws_G = int(set_G[1])
                 # LED.colorWipe(Color(ws_R,ws_G,ws_B))
             except:
                 pass
         elif 'wsB' in data:
             try:
-                set_B=data.split()
+                set_B = data.split()
                 ws_B = int(set_B[1])
                 # LED.colorWipe(Color(ws_R,ws_G,ws_B))
             except:
@@ -263,21 +268,21 @@ def run():
             fpv.FindColor(0)
             fpv.WatchDog(0)
             ultrasonicMode = 0
-            FindLineMode   = 0
-            FindColorMode  = 0
+            FindLineMode = 0
+            FindColorMode = 0
             tcpCliSock.send(('func_end').encode())
             move.motorStop()
 
         else:
             pass
-        #print(data)
+        # print(data)
 
 
 if __name__ == '__main__':
 
     HOST = ''
-    PORT = 10223                              #Define port serial 
-    BUFSIZ = 1024                             #Define buffer size
+    PORT = 10223  # Define port serial
+    BUFSIZ = 1024  # Define buffer size
     ADDR = (HOST, PORT)
     '''
     try:
@@ -287,17 +292,17 @@ if __name__ == '__main__':
         print('Use "sudo pip3 install rpi_ws281x" to install WS_281x package')
         pass
     '''
-    while  1:
+    while 1:
         try:
-            s =socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-            s.connect(("1.1.1.1",80))
-            ipaddr_check=s.getsockname()[0]
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.connect(("1.1.1.1", 80))
+            ipaddr_check = s.getsockname()[0]
             s.close()
             print(ipaddr_check)
         except:
-            ap_threading=threading.Thread(target=ap_thread)   #Define a thread for data receiving
-            ap_threading.setDaemon(True)                          #'True' means it is a front thread,it would close when the mainloop() closes
-            ap_threading.start()                                  #Thread starts
+            ap_threading = threading.Thread(target=ap_thread)  # Define a thread for data receiving
+            ap_threading.setDaemon(True)  # 'True' means it is a front thread,it would close when the mainloop() closes
+            ap_threading.start()  # Thread starts
             '''
             LED.colorWipe(Color(0,16,50))
             time.sleep(1)
@@ -313,17 +318,17 @@ if __name__ == '__main__':
             '''
         try:
             tcpSerSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tcpSerSock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+            tcpSerSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             tcpSerSock.bind(ADDR)
-            tcpSerSock.listen(5)                      #Start server,waiting for client
+            tcpSerSock.listen(5)  # Start server,waiting for client
             print('waiting for connection...')
             tcpCliSock, addr = tcpSerSock.accept()
             print('...connected from :', addr)
 
-            fpv=FPV.FPV()
-            fps_threading=threading.Thread(target=FPV_thread)         #Define a thread for FPV and OpenCV
-            fps_threading.setDaemon(True)                             #'True' means it is a front thread,it would close when the mainloop() closes
-            fps_threading.start()                                     #Thread starts
+            fpv = FPV.FPV()
+            fps_threading = threading.Thread(target=FPV_thread)  # Define a thread for FPV and OpenCV
+            fps_threading.setDaemon(True)  # 'True' means it is a front thread,it would close when the mainloop() closes
+            fps_threading.start()  # Thread starts
             break
         '''
         except:
@@ -333,11 +338,11 @@ if __name__ == '__main__':
             LED.colorWipe(Color(0,80,255))
         '''
         except:
-            pass
+        pass
 
-    try:
-        run()
-    except:
-        # LED.colorWipe(Color(0,0,0))
-        servo.clean_all()
-        move.destroy()
+try:
+    run()
+except:
+    # LED.colorWipe(Color(0,0,0))
+    servo.clean_all()
+    move.destroy()
--- server/servo.py
+++ server/servo.py
@@ -7,16 +7,17 @@
 # Author      : William
 # Date        : 2019/02/23
 from __future__ import division
-import time
-import RPi.GPIO as GPIO
+
 import sys
+import time
+
 import Adafruit_PCA9685
+import RPi.GPIO as GPIO
 
 '''
 change this form 1 to 0 to reverse servos
 '''
 look_direction = 1
-
 
 pwm = Adafruit_PCA9685.PCA9685()
 pwm.set_pwm_freq(50)
@@ -26,67 +27,68 @@ look_min = 100
 
 org_pos = 300
 
+
 def ctrl_range(raw, max_genout, min_genout):
-	if raw > max_genout:
-		raw_output = max_genout
-	elif raw < min_genout:
-		raw_output = min_genout
-	else:
-		raw_output = raw
-	return int(raw_output)
+    if raw > max_genout:
+        raw_output = max_genout
+    elif raw < min_genout:
+        raw_output = min_genout
+    else:
+        raw_output = raw
+    return int(raw_output)
 
 
 def camera_ang(direction, ang):
-	global org_pos
-	if ang == 'no':
-		ang = 50
-	if look_direction:
-		if direction == 'lookdown':
-			org_pos+=ang
-			org_pos = ctrl_range(org_pos, look_max, look_min)
-		elif direction == 'lookup':
-			org_pos-=ang
-			org_pos = ctrl_range(org_pos, look_max, look_min)
-		elif direction == 'home':
-			org_pos = 300
-	else:
-		if direction == 'lookdown':
-			org_pos-=ang
-			org_pos = ctrl_range(org_pos, look_max, look_min)
-		elif direction == 'lookup':
-			org_pos+=ang
-			org_pos = ctrl_range(org_pos, look_max, look_min)
-		elif direction == 'home':
-			org_pos = 300	
+    global org_pos
+    if ang == 'no':
+        ang = 50
+    if look_direction:
+        if direction == 'lookdown':
+            org_pos += ang
+            org_pos = ctrl_range(org_pos, look_max, look_min)
+        elif direction == 'lookup':
+            org_pos -= ang
+            org_pos = ctrl_range(org_pos, look_max, look_min)
+        elif direction == 'home':
+            org_pos = 300
+    else:
+        if direction == 'lookdown':
+            org_pos -= ang
+            org_pos = ctrl_range(org_pos, look_max, look_min)
+        elif direction == 'lookup':
+            org_pos += ang
+            org_pos = ctrl_range(org_pos, look_max, look_min)
+        elif direction == 'home':
+            org_pos = 300
 
-	pwm.set_all_pwm(0,org_pos)
+    pwm.set_all_pwm(0, org_pos)
 
 
 def clean_all():
-	pwm.set_all_pwm(0, 0)
+    pwm.set_all_pwm(0, 0)
 
 
 if __name__ == '__main__':
-	camera_ang('lookup')
-	time.sleep(1)
-	camera_ang('lookup')
-	time.sleep(1)
-	camera_ang('lookup')
-	time.sleep(1)
-	camera_ang('lookup')
-	time.sleep(1)
-	camera_ang('lookdown')
-	time.sleep(1)
-	camera_ang('lookdown')
-	time.sleep(1)
-	camera_ang('home')
-	time.sleep(1)
-	'''
-	camera_ang('home', 0)
-	time.sleep(0.4)
-	clean_all()
-	while 1:
-		a=input('press any key')
-		print(camera_ang('lookup', 0))
-		pass
-	'''
+    camera_ang('lookup')
+    time.sleep(1)
+    camera_ang('lookup')
+    time.sleep(1)
+    camera_ang('lookup')
+    time.sleep(1)
+    camera_ang('lookup')
+    time.sleep(1)
+    camera_ang('lookdown')
+    time.sleep(1)
+    camera_ang('lookdown')
+    time.sleep(1)
+    camera_ang('home')
+    time.sleep(1)
+    '''
+    camera_ang('home', 0)
+    time.sleep(0.4)
+    clean_all()
+    while 1:
+        a=input('press any key')
+        print(camera_ang('lookup', 0))
+        pass
+    '''
