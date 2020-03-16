ce79504983e601ccbbd4bc57258e1b0b3584097f
pi <pi@1a52f2df-19a0-44be-902c-dd0611557c93>
2020-03-16 07:27:45 +0000

    Update log levels.

--- server/speak.py
+++ server/speak.py
@@ -17,15 +17,15 @@ def speak_thread(text):
     while True:
         if config.allow_speak == 1:
             config.allow_speak = 0
-            logger.info('Speaking "%s"', text)
+            logger.debug('Speaking "%s"', text)
             config.last_text = text
             # subprocess.Popen([str('espeak-ng "%s" -s %d' % (text, config.SPEAK_SPEED))], shell=True)
             os.system(str('espeak-ng "%s" -s %d' % (text, config.SPEAK_SPEED)))
             config.allow_speak = 1
             break
         elif text == config.last_text:
-            logger.info('Skipping redundant speech')
+            logger.warning('Skipping redundant speech')
             break
         else:
-            logger.info('Not allowed to speak, waiting...')
+            logger.warning('Not allowed to speak, waiting...')
             time.sleep(0.5)
