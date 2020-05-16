d9e158a319f8919ac2fd2f31c53d90d50e426b42
pi <pi@1a52f2df-19a0-44be-902c-dd0611557c93>
2020-05-16 10:02:26 +0000

    Addded camera angle in config and FPV font size parameters.

--- server/fpv.py
+++ server/fpv.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env/python3
-# File name   : server.py
-# Description : for FPV video and OpenCV functions
-# Website     : www.adeept.com
-# E-mail      : support@adeept.com
-# Author      : William(Based on Adrian Rosebrock's OpenCV code on pyimagesearch.com)
-# Date        : 2019/02/23
+# File name   : fpv.py
+# Description : FPV video and OpenCV functions
+# E-mail      : sect16@gmail.com
+# Author      : Chin Pin Hon (Based on Adrian Rosebrock's OpenCV code on pyimagesearch.com)
+# Date        : 16/05/2020
 
 import argparse
 import base64
@@ -15,19 +14,19 @@ from collections import deque
 import cv2
 import imutils
 import numpy
-import picamera
 import zmq
-from picamera.array import PiRGBArray
+from imutils.video.pivideostream import PiVideoStream
 
 import config
 import led
 import move
 import pid
-import servo
 import speak_dict
 from speak import speak
 
 logger = logging.getLogger(__name__)
+
+led = led.Led()
 pid = pid.Pid()
 pid.SetKp(0.5)
 pid.SetKd(0)
@@ -41,9 +40,14 @@ X_lock = 0
 tor = 17
 FindColorMode = 0
 WatchDogMode = 0
+WATCH_STANDBY = 'blue'
+WATCH_ALERT = 'red'
+frame_image = None
+# Init watchdog variables
+avg = None
+motion_counter = 0
+last_motion_captured = datetime.datetime.now()
 UltraData = 0.45
-
-led = led.Led()
 
 
 class Fpv:
@@ -72,164 +76,47 @@ class Fpv:
         UltraData = invar
 
     def fpv_capture_thread(self, client_ip_address, event):
-        global PORT, footage_socket_client
+        global footage_socket_client, frame_image
+        logger.info('Starting thread')
         ap = argparse.ArgumentParser()  # OpenCV initialization
         ap.add_argument("-b", "--buffer", type=int, default=64,
                         help="max buffer size")
         args = vars(ap.parse_args())
         pts = deque(maxlen=args["buffer"])
-
-        camera = picamera.PiCamera()
-        camera.resolution = config.RESOLUTION
-        camera.framerate = 20
-        rawCapture = PiRGBArray(camera, size=config.RESOLUTION)
-
-        avg = None
-        motionCounter = 0
-        # time.sleep(4)
-        last_motion_captured = datetime.datetime.now()
-
-        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-            if event.is_set():
-                logger.info('Kill event received, terminating FPV thread.')
-                break
-            frame_image = frame.array
-            cv2.line(frame_image, (300, 240), (340, 240), (128, 255, 128), 1)
-            cv2.line(frame_image, (320, 220), (320, 260), (128, 255, 128), 1)
-            timestamp = datetime.datetime.now()
+        vs = PiVideoStream(resolution=config.RESOLUTION, framerate=config.FRAME_RATE).start()
+        # time.sleep(2.0)
+        while not event.is_set():
+            frame_image = vs.read()
+            # Draw crosshair lines
+            cv2.line(frame_image, (int(config.RESOLUTION[0] / 2) - 20, int(config.RESOLUTION[1] / 2)),
+                     (int(config.RESOLUTION[0] / 2) + 20, int(config.RESOLUTION[1] / 2)), (128, 255, 128), 1)
+            cv2.line(frame_image, (int(config.RESOLUTION[0] / 2), int(config.RESOLUTION[1] / 2) - 20),
+                     (int(config.RESOLUTION[0] / 2), int(config.RESOLUTION[1] / 2) + 20), (128, 255, 128), 1)
 
             if FindColorMode:
-                ####>>>OpenCV Start<<<####
-                hsv = cv2.cvtColor(frame_image, cv2.COLOR_BGR2HSV)
-                mask = cv2.inRange(hsv, self.colorLower, self.colorUpper)
-                mask = cv2.erode(mask, None, iterations=2)
-                mask = cv2.dilate(mask, None, iterations=2)
-                cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                        cv2.CHAIN_APPROX_SIMPLE)[-2]
-                center = None
-                if len(cnts) > 0:
-                    cv2.putText(frame_image, 'Target Detected', (40, 60), config.FONT, 0.5, (255, 255, 255), 1,
-                                cv2.LINE_AA)
-                    c = max(cnts, key=cv2.contourArea)
-                    ((x, y), radius) = cv2.minEnclosingCircle(c)
-                    M = cv2.moments(c)
-                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-                    X = int(x)
-                    Y = int(y)
-                    if radius > 10:
-                        cv2.rectangle(frame_image, (int(x - radius), int(y + radius)),
-                                      (int(x + radius), int(y - radius)), (255, 255, 255), 1)
-                    logger.debug('Target input frame position X: %d Y: %d', X, Y)
-                    if Y < (240 - tor):
-                        error = (240 - Y) / 5
-                        outv = int(round((pid.GenOut(error)), 0))
-                        servo.camera_ang('lookup', outv)
-                        Y_lock = 0
-                    elif Y > (240 + tor):
-                        error = (Y - 240) / 5
-                        outv = int(round((pid.GenOut(error)), 0))
-                        servo.camera_ang('lookdown', outv)
-                        Y_lock = 0
-                    else:
-                        Y_lock = 1
+                find_color(self, pts, args)
 
-                    if X < (320 - tor * 3):
-                        move.move(config.SPEED_BASE, 'no', 'left', config.RADIUS)
-                        # time.sleep(0.1)
-                        # move.motorStop()
-                        X_lock = 0
-                    elif X > (330 + tor * 3):
-                        move.move(config.SPEED_BASE, 'no', 'right', config.RADIUS)
-                        # time.sleep(0.1)
-                        # move.motorStop()
-                        X_lock = 0
-                    else:
-                        move.motorStop()
-                        X_lock = 1
-
-                    if X_lock == 1 and Y_lock == 1:
-                        if UltraData > config.MAX_TRACK_DISTANCE:
-                            move.move(config.SPEED_BASE, 'forward', 'no', config.RADIUS)
-                        elif UltraData < config.MIN_TRACK_DISTANCE:
-                            move.move(config.SPEED_BASE, 'backward', 'no', config.RADIUS)
-                            logger.debug('Ultrasonic input data: %s', UltraData)
-                        else:
-                            move.motorStop()
-
-
-                else:
-                    cv2.putText(frame_image, 'Target Detecting', (40, 60), config.FONT, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-                    move.motorStop()
-
-                for i in range(1, len(pts)):
-                    if pts[i - 1] is None or pts[i] is None:
-                        continue
-                    thickness = int(numpy.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-                    cv2.line(frame_image, pts[i - 1], pts[i], (0, 0, 255), thickness)
-                ####>>>OpenCV Ends<<<####
-
-            if WatchDogMode:
-                gray = cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
-                gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-                if avg is None:
-                    logger.info("Starting background model for watchdog...")
-                    avg = gray.copy().astype("float")
-                    rawCapture.truncate(0)
-                    continue
-
-                cv2.accumulateWeighted(gray, avg, 0.5)
-                frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
-                # threshold the delta image, dilate the thresholded image to fill
-                # in holes, then find contours on thresholded image
-                thresh = cv2.threshold(frameDelta, 5, 255,
-                                       cv2.THRESH_BINARY)[1]
-                thresh = cv2.dilate(thresh, None, iterations=2)
-                cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                        cv2.CHAIN_APPROX_SIMPLE)
-                cnts = imutils.grab_contours(cnts)
-                # loop over the contours
-                for c in cnts:
-                    # if the contour is too small, ignore it
-                    if cv2.contourArea(c) < config.MAX_CONTOUR_AREA:
-                        logger.debug('Contour too small (%s), ignoring...', cv2.contourArea(c))
-                        continue
-                    else:
-                        speak(speak_dict.bark)
-
-                    # compute the bounding box for the contour, draw it on the frame,
-                    # and update the text
-                    (x, y, w, h) = cv2.boundingRect(c)
-                    cv2.rectangle(frame_image, (x, y), (x + w, y + h), (128, 255, 0), 1)
-                    text = "Occupied"
-                    motionCounter += 1
-                    logger.info('Motion frame counter: %s', motionCounter)
-                    led.colorWipe([255, 16, 0])
-                    last_motion_captured = timestamp
-                '''
-                if (timestamp - last_motion_captured).seconds >= 0.5:
-                    led.colorWipe(Color(255,255,0))
-                '''
-                if (timestamp - last_motion_captured).seconds >= 0.5:
-                    logger.debug('No motion detected.')
-                    led.colorWipe([255, 255, 0])
+            elif WatchDogMode:
+                watchdog()
 
             if config.VIDEO_OUT == 1:
                 if footage_socket_client is None:
                     logger.info('Initializing ZMQ client...')
                     init_client(client_ip_address)
                 encoded, buffer = cv2.imencode('.jpg', frame_image)
-                jpg_as_text = base64.b64encode(buffer)
+                # with open('buffer.jpg', mode='wb') as file:
+                #     file.write(buffer)
                 # logger.debug('Sending footage using ZMQ')
-                footage_socket_client.send(jpg_as_text)
+                footage_socket_client.send(base64.b64encode(buffer))
             elif config.VIDEO_OUT == 0 and footage_socket_client is not None:
                 logger.info('Terminating ZMQ client.')
                 footage_socket_client.close()
                 footage_socket_client = None
-            rawCapture.truncate(0)
-        logger.debug('Stopping thread')
-        camera.close()
+            if event.is_set():
+                logger.info('Kill event received, terminating FPV thread.')
+                break
+        logger.info('Stopping VIDEO thread')
+        vs.stop()
 
 
 def init_client(client_ip_address):
@@ -241,8 +128,127 @@ def init_client(client_ip_address):
     footage_socket_client.connect('tcp://%s:%d' % (client_ip_address, config.FPV_PORT))
 
 
+def watchdog():
+    global avg, last_motion_captured, motion_counter, frame_image
+    timestamp = datetime.datetime.now()
+    gray = cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    if avg is None:
+        logger.info("Starting background model for watchdog...")
+        avg = gray.copy().astype("float")
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, 5, 255,
+                           cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < config.MAX_CONTOUR_AREA:
+            logger.debug('Contour too small (%s), ignoring...', cv2.contourArea(c))
+            continue
+        else:
+            speak(speak_dict.bark)
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame_image, (x, y), (x + w, y + h), (128, 255, 0), 1)
+        motion_counter += 1
+        logger.info('Motion frame counter: %s', motion_counter)
+        led.color_set(WATCH_ALERT)
+        last_motion_captured = timestamp
+        with open('buffer.jpg', mode='wb') as file:
+            encoded, buffer = cv2.imencode('.jpg', frame_image)
+            file.write(buffer)
+    if (timestamp - last_motion_captured).seconds >= 0.5:
+        logger.debug('No motion detected.')
+        # timer = str(timestamp - last_motion_captured).split('.', 2)[0]
+        # cv2.putText(frame_image, 'No motion detected for ' + timer, (40, 60), config.FONT, config.FONT_SIZE, (255, 255, 255), 1,
+        #             cv2.LINE_AA)
+        led.color_set(WATCH_STANDBY)
+
+
+def find_color(self, pts, args):
+    global Y_lock, X_lock
+    ####>>>OpenCV Start<<<####
+    hsv = cv2.cvtColor(frame_image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, self.colorLower, self.colorUpper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    if len(cnts) > 0:
+        cv2.putText(frame_image, 'Target Detected', (40, 60), config.FONT, config.FONT_SIZE, (255, 255, 255), 1,
+                    cv2.LINE_AA)
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        X = int(x)
+        Y = int(y)
+        if radius > 10:
+            cv2.rectangle(frame_image, (int(x - radius), int(y + radius)),
+                          (int(x + radius), int(y - radius)), (255, 255, 255), 1)
+        logger.debug('Target input frame position X: %d Y: %d', X, Y)
+
+        if Y < ((config.RESOLUTION[1] / 2) - tor):
+            error = ((config.RESOLUTION[1] / 2) - Y) / 5
+            outv = int(round((pid.GenOut(error)), 0))
+            servo.camera_ang('lookup', outv)
+            Y_lock = 0
+        elif Y > ((config.RESOLUTION[1] / 2) + tor):
+            error = (Y - (config.RESOLUTION[1] / 2)) / 5
+            outv = int(round((pid.GenOut(error)), 0))
+            servo.camera_ang('lookdown', outv)
+            Y_lock = 0
+        else:
+            Y_lock = 1
+
+        if X < (config.RESOLUTION[0] / 2 - tor * 3):
+            move.move(config.SPEED_BASE, 'no', 'left', config.RADIUS)
+            # time.sleep(0.1)
+            # move.motorStop()
+            X_lock = 0
+        elif X > (config.RESOLUTION[0] / 2 + tor * 3):
+            move.move(config.SPEED_BASE, 'no', 'right', config.RADIUS)
+            # time.sleep(0.1)
+            # move.motorStop()
+            X_lock = 0
+        else:
+            move.motorStop()
+            X_lock = 1
+
+        if X_lock == 1 and Y_lock == 1:
+            if UltraData > config.MAX_TRACK_DISTANCE:
+                move.move(config.SPEED_BASE, 'forward', 'no', config.RADIUS)
+            elif UltraData < config.MIN_TRACK_DISTANCE:
+                move.move(config.SPEED_BASE, 'backward', 'no', config.RADIUS)
+                logger.debug('Ultrasonic input data: %s', UltraData)
+            else:
+                move.motorStop()
+    else:
+        cv2.putText(frame_image, 'Detecting target', (40, 60), config.FONT, config.FONT_SIZE, (255, 255, 255), 1,
+                    cv2.LINE_AA)
+        led.color_set('yellow')
+
+    for i in range(1, len(pts)):
+        if pts[i - 1] is None or pts[i] is None:
+            continue
+        thickness = int(numpy.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+        cv2.line(frame_image, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    ####>>>OpenCV Ends<<<####
+
+
 if __name__ == '__main__':
+    import threading
+
+    event = threading.event
+    event.is_set = False
     fpv = Fpv()
     while 1:
-        fpv.fpv_capture_thread('127.0.0.1')
+        fpv.fpv_capture_thread('127.0.0.1', event)
         pass
